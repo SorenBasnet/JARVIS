2c3217f10ab0e19817ac6157a803530e3e0002b3
SorenBasnet <64244617+SorenBasnet@users.noreply.github.com>
2022-09-14 09:26:13 -0500

    Create jarvis.py

---
+++ jarvis.py
@@ -0,0 +1,63 @@
+import speech_recognition as sr 
+import pyttsx3
+import pyaudio
+
+def voiceAction(command):
+    pass  
+
+
+def SpeakText(command):
+     
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+     
+     
+def SpeechRecognition(): 
+    r = sr.Recognizer() 
+     
+    # Exception handling to handle
+    # exceptions at the runtime
+    try:
+         
+        # use the microphone as source for input.
+        with sr.Microphone() as source2:
+             
+            # wait for a second to let the recognizer
+            # adjust the energy threshold based on
+            # the surrounding noise level
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+             
+            #listens for the user's input
+            audio2 = r.listen(source2)
+             
+            # Using google to recognize audio
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+ 
+            print("Did you say "+MyText)
+            SpeakText(MyText)
+            user_confirmation = input("Y/N")
+            if user_confirmation.lower() == "y":
+
+                voiceAction(MyText)
+
+            else: 
+
+                SpeakText("Lets Try That again then.")
+
+                SpeechRecognition()
+            
+
+             
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+         
+    except sr.UnknownValueError:
+        print("unknown error occured")
+
+
+
+
+
